894912770494def96efe969c47486a48d81c7be2
Ilias Sachpazidis <ilias.sachpazidis@gmail.com>
2021-06-06 23:21:31 +0200

    Get_Records_With_FollowUp_Time_Less_Than_12_Months_And_Fractions_LessOrEqual_6_Test() added

--- CsvHelperExtensions.Tests/DataRepositoryServiceWithSpecsTests.cs
+++ CsvHelperExtensions.Tests/DataRepositoryServiceWithSpecsTests.cs
@@ -1,5 +1,6 @@
 ﻿using System.Linq;
 using NUnit.Framework;
+using Vouzamo.Specification;
 
 namespace CsvHelperExtensions.Tests
 {
@@ -26,6 +27,18 @@ namespace CsvHelperExtensions.Tests
         {
             var records = _repositoryService.Get(new WithMaxFollowUpTimeSpec(12));
             Assert.AreEqual(3, records.Count());
+        } 
+        
+        [Test]
+        public void Get_Records_With_FollowUp_Time_Less_Than_12_Months_And_Fractions_LessOrEqual_6_Test()
+        {
+            var records = _repositoryService.Get(new WithMaxFollowUpTimeSpec(12).And(new WithFractionsLessOrEqualTo(6)));
+            foreach (var item in records)
+            {
+                Assert.Less(item.FollowUpTimeInMonths, 12);
+                Assert.LessOrEqual(item.Fractions, 6);
+            }
+
         }
     }
 }
---
+++ CsvHelperExtensions.Tests/WithFractionsLessOrEqualTo.cs
@@ -0,0 +1,19 @@
+﻿using Vouzamo.Specification.Interfaces;
+
+namespace CsvHelperExtensions.Tests
+{
+    public class WithFractionsLessOrEqualTo : ISpecification<TreatmentItem>
+    {
+        private double _fractions;
+
+        public WithFractionsLessOrEqualTo(double fractions)
+        {
+            _fractions = fractions;
+        }
+
+        public bool IsSatisfiedBy(TreatmentItem subject)
+        {
+            return subject.Fractions <= _fractions;
+        }
+    }
+}
